ae8ac1a269976dc8b115f00ff27a0dd7d58356e1
felixfrdd <148837128+felixfrdd@users.noreply.github.com>
2023-11-15 15:37:01 +0700

    save #3

--- lib/main.dart
+++ lib/main.dart
@@ -13,215 +13,251 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color.fromARGB(238, 255, 255, 255),
-        appBar: AppBar(
-          leading: const Icon(Icons.arrow_back),
-          title: const Text(
-            'KomputerKu',
+          backgroundColor: const Color.fromARGB(238, 255, 255, 255),
+          appBar: AppBar(
+            leading: const Icon(Icons.arrow_back),
+            title: const Text(
+              'KomputerKu',
+            ),
+            backgroundColor: Colors.red,
           ),
-          backgroundColor: Colors.red,
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            const Padding(padding: EdgeInsets.only(top: 15.0)),
-            Align(
-              child: Container(
-                height: 100,
-                width: 300,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/computershop_icon.png',
-                      height: 50,
-                      width: 100,
+          body: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Padding(padding: EdgeInsets.only(top: 15.0)),
+                Align(
+                  child: Container(
+                    height: 100,
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
                     ),
-                    const SizedBox(width: 8.0),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
+                    child: Row(
                       children: [
-                        const Padding(
-                          padding: EdgeInsets.only(top: 8.0, left: 5.0),
-                          child: Text(
-                            'KomputerKu',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
+                        Image.asset(
+                          'assets/images/computershop_icon.png',
+                          height: 50,
+                          width: 100,
                         ),
-                        const Padding(
-                          padding: EdgeInsets.only(top: 8.0),
-                          child: Row(
-                            children: [
-                              Icon(
-                                Icons.person,
-                                color: Colors.grey,
-                                size: 20,
-                              ),
-                              Text(
-                                '11.252 pengikut',
-                                style: TextStyle(fontSize: 12),
-                              )
-                            ],
-                          ),
-                        ),
-                        Row(
+                        const SizedBox(width: 8.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            TextButton(
-                              onPressed: () {},
-                              style: ButtonStyle(
-                                side: MaterialStateProperty.all<BorderSide>(
-                                  const BorderSide(
-                                      color: Colors.blue, width: 2.0),
-                                ),
-                                shape: MaterialStateProperty.all<
-                                    RoundedRectangleBorder>(
-                                  RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(10.0),
-                                  ),
-                                ),
-                              ),
-                              child: const Text(
-                                'Mengikuti',
-                                style: TextStyle(
-                                  fontSize: 12.0,
-                                ),
+                            const Padding(
+                              padding: EdgeInsets.only(top: 8.0, left: 5.0),
+                              child: Text(
+                                'KomputerKu',
+                                style: TextStyle(fontWeight: FontWeight.bold),
                               ),
                             ),
-                            const Icon(
-                              Icons.share,
-                              color: Colors.grey,
-                              size: 20,
+                            const Padding(
+                              padding: EdgeInsets.only(top: 8.0),
+                              child: Row(
+                                children: [
+                                  Icon(
+                                    Icons.person,
+                                    color: Colors.grey,
+                                    size: 20,
+                                  ),
+                                  Text(
+                                    '11.252 pengikut',
+                                    style: TextStyle(fontSize: 12),
+                                  )
+                                ],
+                              ),
+                            ),
+                            Row(
+                              children: [
+                                TextButton(
+                                  onPressed: () {},
+                                  style: ButtonStyle(
+                                    side: MaterialStateProperty.all<BorderSide>(
+                                      const BorderSide(
+                                          color: Colors.blue, width: 2.0),
+                                    ),
+                                    shape: MaterialStateProperty.all<
+                                        RoundedRectangleBorder>(
+                                      RoundedRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                    ),
+                                  ),
+                                  child: const Text(
+                                    'Mengikuti',
+                                    style: TextStyle(
+                                      fontSize: 12.0,
+                                    ),
+                                  ),
+                                ),
+                                const Icon(
+                                  Icons.share,
+                                  color: Colors.grey,
+                                  size: 20,
+                                )
+                              ],
                             )
                           ],
                         )
                       ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 20.0),
-            ),
-            Center(
-              child: CarouselSlider(
-                items: [
-                  Image.asset('assets/images/carousel_1.jpg',
-                      fit: BoxFit.cover),
-                  Image.asset('assets/images/carousel_2.jpg',
-                      fit: BoxFit.cover),
-                  Image.asset('assets/images/carousel_3.jpg',
-                      fit: BoxFit.cover),
-                ],
-                options: CarouselOptions(
-                  height: 200.0,
-                  enlargeCenterPage: true,
-                  autoPlay: true,
-                  enableInfiniteScroll: true,
-                  autoPlayInterval: Duration(seconds: 3),
-                  autoPlayAnimationDuration: Duration(milliseconds: 800),
-                ),
-              ),
-            ),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.only(top: 15.0)),
-                  Text(
-                    'Belanja berdasarkan kategori',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
                     ),
-                  )
-                ],
-              ),
-            ),
-            Row(
-              children: [
-                Expanded(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.only(top: 20.0),
+                ),
+                Center(
+                  child: CarouselSlider(
+                    items: [
+                      Image.asset('assets/images/carousel_1.jpg',
+                          fit: BoxFit.cover),
+                      Image.asset('assets/images/carousel_2.jpg',
+                          fit: BoxFit.cover),
+                      Image.asset('assets/images/carousel_3.jpg',
+                          fit: BoxFit.cover),
+                    ],
+                    options: CarouselOptions(
+                      height: 200.0,
+                      enlargeCenterPage: true,
+                      autoPlay: true,
+                      enableInfiniteScroll: true,
+                      autoPlayInterval: Duration(seconds: 3),
+                      autoPlayAnimationDuration: Duration(milliseconds: 800),
+                    ),
+                  ),
+                ),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
                     children: [
-                      Image.asset(
-                        'assets/images/category_1.jpg',
-                        height: 100,
-                        width: 100,
-                      ),
-                      Image.asset(
-                        'assets/images/category_2.jpg',
-                        height: 100,
-                        width: 100,
-                      ),
-                      Image.asset(
-                        'assets/images/category_3.jpg',
-                        height: 100,
-                        width: 100,
-                      ),
-                      Image.asset(
-                        'assets/images/category_4.jpg',
-                        height: 100,
-                        width: 100,
+                      Padding(padding: EdgeInsets.only(top: 15.0)),
+                      Text(
+                        'Belanja berdasarkan kategori',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
                       )
                     ],
                   ),
-                )
-              ],
-            ),
-            const Align(
-              alignment: Alignment.topLeft,
-              child: Column(
-                children: [
-                  Padding(padding: EdgeInsets.only(top: 15.0)),
-                  Text(
-                    'Semua Produk',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.w900,
+                ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset(
+                            'assets/images/category_1.jpg',
+                            height: 100,
+                            width: 100,
+                          ),
+                          Image.asset(
+                            'assets/images/category_2.jpg',
+                            height: 100,
+                            width: 100,
+                          ),
+                          Image.asset(
+                            'assets/images/category_3.jpg',
+                            height: 100,
+                            width: 100,
+                          ),
+                          Image.asset(
+                            'assets/images/category_4.jpg',
+                            height: 100,
+                            width: 100,
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+                const Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    children: [
+                      Padding(padding: EdgeInsets.only(top: 15.0)),
+                      Text(
+                        'Semua Produk',
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.w900,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/images/product_1.jpg',
+                            height: 200,
+                            width: 200,
+                          ),
+                          Text('TUF GAMING'),
+                          Text('Rp 18.000.000'),
+                        ],
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                Expanded(
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'assets/images/product_1.jpg',
-                        height: 200,
-                        width: 200,
+                    const SizedBox(width: 5.0),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/images/product_2.jpg',
+                            height: 200,
+                            width: 200,
+                          ),
+                          Text('MSI'),
+                          Text('Rp 14.000.000'),
+                        ],
                       ),
-                      Text('TUF GAMING')
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
-                const SizedBox(width: 5.0),
-                Expanded(
-                  child: Column(
-                    children: [
-                      Image.asset(
-                        'assets/images/product_2.jpg',
-                        height: 200,
-                        width: 200,
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/images/product_3.jpg',
+                            height: 200,
+                            width: 200,
+                          ),
+                          Text('ASUS ROG ALLY'),
+                          Text('Rp 11.000.000'),
+                        ],
                       ),
-                      Text('MSI')
-                    ],
-                  ),
+                    ),
+                    const SizedBox(width: 5.0),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Image.asset(
+                            'assets/images/product_4.jpg',
+                            height: 200,
+                            width: 200,
+                          ),
+                          Text('Lenovo Legion'),
+                          Text('Rp 20.000.000'),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
               ],
             ),
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
